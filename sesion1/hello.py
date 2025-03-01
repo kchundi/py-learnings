738829da75dc6d0bbd5ef8d41cee784b42879120
kchundi <kchundi@gmail.com>
2025-03-01 12:28:04 -0500

    added sesion1 files

---
+++ sesion1/hello.py
@@ -0,0 +1,27 @@
+a = 5
+b = 'python'
+c = "pythonpython"
+d = 5.5
+e = f = d
+print(a,"Print A value ")
+print (type(a), "type(a)")
+
+print(b,"Print B value ")
+print (type(b), "type(b)")
+
+print(c,"Print C value ")
+print (type(c), "type(c)")
+
+print(d,"Print D value ")
+print (type(d), "type(d")
+
+print(e,"Print E value ")
+print (type(e), "type(e)")
+
+e,f = 4.5           
+
+print(e,"Print e alue ")
+print (type(e), "type(e)")
+
+print(f,"Print F value ")
+print (type(f), "type(  e)")
